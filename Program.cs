9afdc5326cd3cdd0f522e2176bed0191c0e75276
solvemate2018 <35694266+solvemate2018@users.noreply.github.com>
2018-01-24 19:12:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace basic_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convertor from decimal to binar");
+            Console.Write("Please enter a number:");
+            var num = int.Parse(Console.ReadLine());
+
+            for (int i = num; i > 1;)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine("1");
+                    i /= 2;
+                }
+                else
+                {
+                    Console.WriteLine("0");
+                    i /= 2;
+                }
+            }
+            Console.WriteLine("1");
+        }
+    }
+}
